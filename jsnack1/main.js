0d1efdcf7e19259690ad417e4522985f41314d37
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-12 11:39:17 +0200

    end snack1

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -1,1 +1,20 @@
 console.log("hello");
+
+//L’utente inserisce due numeri in successione, con due prompt.
+//Il software stampa il maggiore.
+
+const firstNumber = prompt("Inserisci il tuo primo numero")
+const secondNumber = prompt("Inserisci il tuo secondo numero")
+
+const result = document.querySelector("h1")
+
+if (firstNumber > secondNumber){
+    console.log(firstNumber);
+    result.innerHTML = firstNumber
+} else if (secondNumber > firstNumber){
+    console.log(secondNumber)
+    result.innerHTML = secondNumber
+} else {
+    console.log("both");
+    result.innerHTML = "both"
+}
